50aebb618cf1eab395660c5fb9ff0b983fd12a5a
James Wilson <me@unbui.lt>
2018-05-22 22:26:47 +0100

    add instructions on successful load

--- js/index.js
+++ js/index.js
@@ -466,6 +466,9 @@ function FractalWorker(){
 
 // Run everything. If we fail to initialise workers, display
 // no support message.
-init().catch(_ => {
+init().then(_ => {
+	displayInstructions();
+	document.addEventListener("keydown", hideInstructions);
+}, _ => {
 	displayNoSupport("I'm sorry, but there was an issue fetching or instantiating the WASM based Web Workers. Do you have a plugin enabled that blocks WASM?");
 })
